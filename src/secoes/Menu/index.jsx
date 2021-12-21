fd8e93bdc9dae5934fa7ea69413b05a022325225
mardefronteira <mmv341@nyu.edu>
2021-12-21 10:58:50 -0300

    lógica de língua + alteração do local das pop ups

--- src/secoes/Menu/index.jsx
+++ src/secoes/Menu/index.jsx
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Botao from '../../componentes/Botao';
 import { Conteiner, NavCentral } from './estilo';
+import {
+  PopUpLivro,
+  PopUpCreditos,
+  PopUpAudiodescricao,
+  PopUpDownload
+} from '../../popups';
 
 function Menu() {
+  const [abrirAudiodesc, setAbrirAudiodesc] = useState(false);
+  const [abrirCreditos, setAbrirCreditos] = useState(false);
+  const [abrirDownload, setAbrirDownload] = useState(false);
+  const [abrirLivro, setAbrirLivro] = useState(false);
   
   return (
     <Conteiner>
-    <p>Entre os anos de 2015 e 2020, a Galeria Periscópio ocupou uma casa tombada na Av. Álvares Cabral no 534, em Belo Horizonte, onde realizou 28 exposições. Este livro configura um arquivo das atividades que ocorreram nesse período.</p>
+      <p>Entre os anos de 2015 e 2020, a Galeria Periscópio ocupou uma casa tombada na Av. Álvares Cabral no 534, em Belo Horizonte, onde realizou 28 exposições. Este livro configura um arquivo das atividades que ocorreram nesse período.</p>
 
-    <Botao pos={{x: 'esquerda', y: 'superior'}}>
-      Download Livro
-    </Botao>
-
-    <Botao pos={{x: 'esquerda', y: 'inferior'}}>
-      Galeria Periscópio
-    </Botao>
-
-    <NavCentral>
-      <Botao>
-        Livro
-      </Botao> 
-      <Botao>
-        Artigos
-      </Botao> 
-      <Botao>
-        Info
+      <Botao pos={{x: 'esquerda', y: 'superior'}}>
+        Download Livro
       </Botao>
-    </NavCentral>
 
-    <Botao pos={{x: 'direita', y: 'inferior'}}>
-      EN/PT
-    </Botao>
+      <Botao pos={{x: 'esquerda', y: 'inferior'}}>
+        Galeria Periscópio
+      </Botao>
 
-    <Botao pos={{x: 'direita', y: 'superior'}}>
-      Audiodescrição
-    </Botao>
+      <NavCentral>
+        <Botao>
+          Livro
+        </Botao> 
+        <Botao>
+          Artigos
+        </Botao> 
+        <Botao>
+          Info
+        </Botao>
+      </NavCentral>
+
+      <Botao pos={{x: 'direita', y: 'inferior'}}>
+        EN/PT
+      </Botao>
+
+      <Botao pos={{x: 'direita', y: 'superior'}}>
+        Audiodescrição
+      </Botao>
+
+      <PopUpLivro />
+      <PopUpCreditos />
+      <PopUpAudiodescricao />
+      <PopUpDownload />  
     </Conteiner>
   );
 }
