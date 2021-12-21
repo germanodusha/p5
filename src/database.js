ede0fe2005c781d6d8e5bb85201cffd8dc250b0e
Jainy Gomes <andreajgsouza@hotmail.com>
2021-12-21 10:05:09 -0300

    Database em duas linguas

--- src/database.js
+++ src/database.js
@@ -150,11 +150,11 @@ const dataBase = [{
             Como ativadora da cena da cultura local e ponto de articulação entre os atores e o público de todo o país, a Periscópio busca contribuir para o desenvolvimento de um imaginário social diverso e efervescente, com a firme crença na prática artística como chave de transformação positiva do indivíduo e da sociedade.
             `,
             fichaTecnica:[{
-                titulo:'Um projeto A project by:',
+                titulo:'Um projeto:',
                 autoria:'Galeria Periscópio'
             },
             {
-             titulo: 'Realização Produced by:',
+             titulo: 'Realização:',
              autoria:`Secretária Especial da Cultura
              Ministério do Turismo
              `
@@ -164,11 +164,11 @@ const dataBase = [{
             autoria:'Agropel; Nova Catalão, Tecar Fiat'
             },
             {
-            titulo:'Sócios-proprietários Partners directors:',
+            titulo:'Sócios-proprietários:',
             autoria:'Alexandre Romanini; Altivo Duarte; Rodrigo Mitre'
             },
             {
-            titulo: 'Direção Direction',
+            titulo: 'Direção',
             autoria:'Rodrigo Mitre'
             },
             {
@@ -176,39 +176,39 @@ const dataBase = [{
             autoria:'Fora/Genesys'
             },
             {
-            titulo: 'Organização e edição Conception and edited by: ',
+            titulo: 'Organização e edição: ',
             autoria:'Germano Dushá'
             },
             {
-            titulo:'Coordenação Coordination:',
+            titulo:'Coordenação:',
             autoria:'Fabricia Ramos; Germano Dushá'
             },
             {
-            titulo:'Ensaios Essays:',
+            titulo:'Ensaios:',
             autoria:'Alexandre Romanini, Altivo Duarte e Rodrigo Mitre; Germano Dushá; Libéria Neves e Vicente Camiloti; Raphael Fonseca'
             },
             {
-            titulo: 'Design gráfico Graphic design:',
+            titulo: 'Design gráfico:',
             autoria:'Raul Luna' 
             },
             {
-            titulo:'Finalização de conteúdo Editorial post-production:',
+            titulo:'Finalização de conteúdo:',
             autoria:'Marcelo Mudou'
             },
             {
-             titulo: 'Produção Production:',
+             titulo: 'Produção:',
              autoria:'Julia Costa; Bruna Cabeço'
             },
             {
-            titulo:'Tradução Translation:',
+            titulo:'Tradução:',
             autoria:'Fabricia Ramos; Patricia Porto'
             },
             {
-            titulo:'Preparação de texto (ensaios) Copyediting (essays):',
+            titulo:'Preparação de texto (ensaios):',
             autoria:'Sandra Brazil'
             },
             {
-            titulo: 'Registros fotográficos Photographic documentation:',
+            titulo: 'Registros fotográficos:',
             autoria:'Eduardo Eckenfels; Everton Ballardin; Pat Kilgore; Rodrigo Albert'
             },
             {
@@ -216,7 +216,7 @@ const dataBase = [{
             autoria:'Alessandra Ferreira; André Victor; Franimar Araújo; Gustavo Germano; Isabela Barros; John Silveira; José Abel Catarina; Julia Costa; Lucas Lima; Rafael Boneco; Vanessa Monteze'
             },
             {
-            titulo: 'Agradecimentos Acknowledgments:',
+            titulo: 'Agradecimentos:',
             autoria:'Alberto Pezeiro; Alessandra Mitre; Alex Schott; Alexandre Meira; Álvaro Piquet; Augusto Nunes;  Beatriz de Almeida Magalhães; Beatriz Menezes; Breno Schott; Bruno Assumpção; Bruno Bedinelli; Carolina Bortoletto; Cristiana Mitre; Fabiano Galdino; Fabíola Moulin; Felice Filmes; Felipe Scovino; Flávio Reis; Francisco Gracindo; Frederico Morais; Galeria Celma Albuquerque; Galeria Fortes D’Aloia & Gabriel; Galeria Jaqueline Martins; Galeria Mendes Wood DM; Hamilton Mitre; Hallison Moreira; Hélio Lauar; Hélio Menezes; Jarbas Velloso; João Guilherme Dayrell; Joelina Santos; José Élcio Monteze; Júlio Martins; Katia Canton; Leonardo Bortoletto; Libéria Neves; Lucas e Luquinas; Lúcio Chamon; Luiz Flávio; Luiz Rodrigo Cerqueira; Manoel Macedo; Marcelo Drummond; Marcelo Costa; Marconi Drummond; Maria Clara Godoy; Maria de Lourdes Macedo; Maria Eduarda Pacheco; Maria Montero; Maria Regina Costa; Mariana Abrao; Marina Câmara; Mateus Gontijo; Mônica Grandchamp; Nydia Negromonte; Odete Romanini; Paula Borghi; Paulo Baptista; Paulo Caetano; Pedro Mendes; Prof. Dr. Auterives Maciel Jr; Raimundo Jorge Mourão; Raphael Fonseca; Raquel Cecília de Oliveira; Renata Marquez; Renata Montagneri; Renata Romanini; Renato Silva; Samuel Lacerda; Sé Galeria; Sérgio Martins; Sérgio Mattos; Silvia Batista, Talita Meira; Uiara Azevedo; Vanessa Costa; Vicente Camiloti; Victor Galvão; Vinicius Veloso; Virginia Geo; Wagner Nardy; Wilson Lázaro; Wlins Amaranto; Zeca Camargo; Zezé Monteze'
             },
             {
@@ -227,10 +227,232 @@ const dataBase = [{
         }
         
     },
-    en:[
-        {
-
+    en:{
+        principal:{
+            titulo: '',
+            data: '',
+            epigrafe: '',
+            fraseChamada: '',
+            artistasECuradores: ''
+        },
+        index:{
+            titulo:'About Periscopio',
+            ensaios:{
+                titulo:'Essays',
+                itens:[{
+                    titulo:'Ampliando a visão',
+                    autoria:'Alexandre Romanini, Altivo Duarte e Rodrigo Mitr'
+                },
+                {
+                 titulo: 'Programa para transver o mundo',
+                 autoria:'Germano Dushá'
+                },
+                {
+                titulo:'Arte – pensamento em ato',
+                autoria:'Libéria Neves e Vicente Camiloti'
+                },
+                {
+                titulo:'Apenas o começo',
+                autoria:'Raphael Fonseca'
+                }],
+            },
+            exposicoes:{
+                titulo:'Exposições',
+                itens:[{
+                    titulo:'Domingo',
+                    autoria:'Fabio Tremonte'
+                },
+                {
+                 titulo: 'Da Morte e Do Amor',
+                 autoria:'Marc Davi'
+                },
+                {
+                titulo:'Hiperlink',
+                autoria:'Andrea Brown, Camila Lacerda, Márcio Diegues'
+                },
+                {
+                titulo:'Todos os tempos',
+                autoria:'Marcone Moreira'
+                },
+                {
+                titulo: 'Você é a seta',
+                autoria:'Éder Oliveira'
+                },
+                {
+                titulo:'Daquilo que é próprio',
+                autoria:'Marcelo Drummond'
+                },
+                {
+                titulo: 'Todos por Um',
+                autoria:'Curadoria Wilson Lázaro'
+                },
+                {
+                titulo:'Um Piano na Selva',
+                autoria:'Curadoria Germano Dushá'
+                },
+                {
+                titulo:'Projeto Wilson Baptista',
+                autoria:'Wilson Baptista'
+                },
+                {
+                titulo: 'Sem Título (Do Impossível ao Porvir)',
+                //no arquivo cada nome abaixo está em uma linha
+                autoria:'Marina Câmara, Janelas Indiscretas, Guto Lacaz' 
+                },
+                {
+                titulo:'Exposição de Galeria + Anotações sobre a prática',
+                autoria:'Ana Linnemann'
+                },
+                {
+                 titulo: 'Da diversidade vivemos',
+                 autoria:'Acervo'
+                },
+                {
+                titulo:'A cidade onde envelheço',
+                autoria:'Rafael RG'
+                },
+                {
+                titulo:'Luas, Brutos e Sóis',
+                autoria:'Gisele Camargo'
+                },
+                {
+                titulo: 'Olhar Instantâneo',
+                autoria:'Acervo'
+                },
+                {
+                titulo:'Daquilo que é próprio',
+                autoria:'Marcelo Drummond'
+                },
+                {
+                titulo: 'À porta de casa',
+                autoria:'Fábio Baroli'
+                },
+                {
+                titulo:'República da Cobra',
+                autoria:'Randolpho Lamonier e Thiago Martins de Melo'
+                },
+                {
+                titulo:'Toda tentativa é um fracasso',
+                autoria:'Alice Ricci'
+                },
+                {
+                titulo: 'Plano e risco',
+                autoria:'Eduardo Hargreaves' 
+                },
+                {
+                titulo:'Somos todos riscadores',
+                autoria:'Fábio Tremonte'
+                },
+                {
+                titulo:'A parte pelo todo',
+                autoria:'Lucas Dupin'
+                },
+                {
+                 titulo: 'Como ativar os estilhaços da história pela linguagem: re-alfabetização política urgente',
+                 autoria:'Traplev'
+                },
+                {
+                titulo:'Sangue sem voz',
+                autoria:'Umberto Costa Barros'
+                },
+                {
+                titulo:'A escultura no flagrante da ação',
+                autoria:'Flávio Cerqueira'
+                },
+                {
+                titulo: 'Corpo rasgado em estado de céu aberto',
+                autoria:'Luana Vitra'
+                },
+                {
+                titulo:'Memórias são histórias da pele',
+                autoria:'Selma Parreira'
+                },
+                {
+                titulo: 'Um erro inesperado aconteceu',
+                autoria:'Curadoria Marcelo Drummond e Nydia Negromonte'
+                },
+                ],
+            },
+            sobrePeriscopio:`Fundada em Belo Horizonte, em 2015, pelos sócios Alexandre Romanini, Altivo Duarte e Rodrigo Mitre, a Galeria Periscópio traz em seu nome a reverência ao instrumento óptico que permite enxergar por cima de um obstáculo. Orientada pela premissa da arte como vetor de expansão da visão, a galeria tem como objetivo colaborar com o panorama contemporâneo por meio de ações que deslocam o olhar e fazem ver além das barreiras impostas pelo cotidiano.
+            Com um programa pautado pelo vigor da invenção estética e engajado às urgências correntes, a Periscópio apresenta mostras coletivas e exposições individuais privilegiando a pluralidade de ideias, de meios e formas, a fim de criar situações instigantes, fomentando o pensamento crítico e o debate produtivo. De jovens artistas ainda nos primeiros impulsos de suas trajetórias a nomes consolidados e importantes agentes de décadas passadas, a galeria trabalha para dar vazão ao que ainda nos é desconhecido, bem como para propor novas abordagens e leituras para o que já se encontra diante dos olhos.
+            Como ativadora da cena da cultura local e ponto de articulação entre os atores e o público de todo o país, a Periscópio busca contribuir para o desenvolvimento de um imaginário social diverso e efervescente, com a firme crença na prática artística como chave de transformação positiva do indivíduo e da sociedade.
+            `,
+            fichaTecnica:[{
+                titulo:'A project by:',
+                autoria:'Galeria Periscópio'
+            },
+            {
+             titulo: 'Produced by:',
+             autoria:`Secretária Especial da Cultura
+             Ministério do Turismo
+             `
+            },
+            {
+            titulo:'Patrocínio:',
+            autoria:'Agropel; Nova Catalão, Tecar Fiat'
+            },
+            {
+            titulo:'Partners directors:',
+            autoria:'Alexandre Romanini; Altivo Duarte; Rodrigo Mitre'
+            },
+            {
+            titulo: 'Direction',
+            autoria:'Rodrigo Mitre'
+            },
+            {
+            titulo:'Coordenação editorial',
+            autoria:'Fora/Genesys'
+            },
+            {
+            titulo: 'Conception and edited by: ',
+            autoria:'Germano Dushá'
+            },
+            {
+            titulo:'Coordination:',
+            autoria:'Fabricia Ramos; Germano Dushá'
+            },
+            {
+            titulo:'Essays:',
+            autoria:'Alexandre Romanini, Altivo Duarte e Rodrigo Mitre; Germano Dushá; Libéria Neves e Vicente Camiloti; Raphael Fonseca'
+            },
+            {
+            titulo: 'Graphic design:',
+            autoria:'Raul Luna' 
+            },
+            {
+            titulo:'Editorial post-production:',
+            autoria:'Marcelo Mudou'
+            },
+            {
+             titulo: 'Production:',
+             autoria:'Julia Costa; Bruna Cabeço'
+            },
+            {
+            titulo:'Translation:',
+            autoria:'Fabricia Ramos; Patricia Porto'
+            },
+            {
+            titulo:'Copyediting (essays):',
+            autoria:'Sandra Brazil'
+            },
+            {
+            titulo: 'Photographic documentation:',
+            autoria:'Eduardo Eckenfels; Everton Ballardin; Pat Kilgore; Rodrigo Albert'
+            },
+            {
+            titulo:'Equipe Periscópio Team (2015-2021):',
+            autoria:'Alessandra Ferreira; André Victor; Franimar Araújo; Gustavo Germano; Isabela Barros; John Silveira; José Abel Catarina; Julia Costa; Lucas Lima; Rafael Boneco; Vanessa Monteze'
+            },
+            {
+            titulo: 'Acknowledgments:',
+            autoria:'Alberto Pezeiro; Alessandra Mitre; Alex Schott; Alexandre Meira; Álvaro Piquet; Augusto Nunes;  Beatriz de Almeida Magalhães; Beatriz Menezes; Breno Schott; Bruno Assumpção; Bruno Bedinelli; Carolina Bortoletto; Cristiana Mitre; Fabiano Galdino; Fabíola Moulin; Felice Filmes; Felipe Scovino; Flávio Reis; Francisco Gracindo; Frederico Morais; Galeria Celma Albuquerque; Galeria Fortes D’Aloia & Gabriel; Galeria Jaqueline Martins; Galeria Mendes Wood DM; Hamilton Mitre; Hallison Moreira; Hélio Lauar; Hélio Menezes; Jarbas Velloso; João Guilherme Dayrell; Joelina Santos; José Élcio Monteze; Júlio Martins; Katia Canton; Leonardo Bortoletto; Libéria Neves; Lucas e Luquinas; Lúcio Chamon; Luiz Flávio; Luiz Rodrigo Cerqueira; Manoel Macedo; Marcelo Drummond; Marcelo Costa; Marconi Drummond; Maria Clara Godoy; Maria de Lourdes Macedo; Maria Eduarda Pacheco; Maria Montero; Maria Regina Costa; Mariana Abrao; Marina Câmara; Mateus Gontijo; Mônica Grandchamp; Nydia Negromonte; Odete Romanini; Paula Borghi; Paulo Baptista; Paulo Caetano; Pedro Mendes; Prof. Dr. Auterives Maciel Jr; Raimundo Jorge Mourão; Raphael Fonseca; Raquel Cecília de Oliveira; Renata Marquez; Renata Montagneri; Renata Romanini; Renato Silva; Samuel Lacerda; Sé Galeria; Sérgio Martins; Sérgio Mattos; Silvia Batista, Talita Meira; Uiara Azevedo; Vanessa Costa; Vicente Camiloti; Victor Galvão; Vinicius Veloso; Virginia Geo; Wagner Nardy; Wilson Lázaro; Wlins Amaranto; Zeca Camargo; Zezé Monteze'
+            },
+            {
+            titulo:'Impressão:',
+            autoria:'Rona Editora'
+            }],
+            notaFinal:'Este livro foi composto utilizando as fontes ES Build, Interval e Inter; e impresso em papel Couchê Brilho 150 grs, Eurobulk 135 grs e Color Plus 80 grs pela Rona Editora, em dezembro de 2021.', 
         }
-    ]
+    }
 
 }]
