0a749237f60a1df1f1e954c96f93ae158d31f6d9
Jainy Gomes <andreajgsouza@hotmail.com>
2021-12-22 10:05:55 -0300

    artigos inseridos na pagina de artigos

--- src/secoes/Artigos/index.jsx
+++ src/secoes/Artigos/index.jsx
@@ -3,10 +3,10 @@ import {  } from './estilo';
 import database from '../../database'
 
 function Artigos() {
-  const artigos = database.ptbr.artigos;
+  const artigos = database.ptbr.index.ensaios.itens;
   return (
     <>
-    {artigos.map((artigo) =>(<>{artigo}</>))}
+    {artigos.map((artigo) =>(<><h3>{artigo.titulo}</h3><h2>{artigo.autoria}</h2><article>{artigo.texto}</article></>))}
     </>
   );
 }
