80c516bbd6f98c88eb14bd6425a4259046453bc5
Jainy Gomes <andreajgsouza@hotmail.com>
2021-12-23 12:06:17 -0300

    Import de imagens do livro e inserção na poplivro

--- src/imagens/livro/index.js
+++ src/imagens/livro/index.js
@@ -1,6 +1,52 @@
-import Foto1 from './livro(1).jpg';
-import Foto2 from './livro(2).jpg';
-import Foto3 from './livro(3).jpg';
-import Foto4 from './livro(4).jpg';
+import Foto1 from './livro_p5_00001.jpg';
+import Foto2 from './livro_p5_00002.jpg';
+import Foto3 from './livro_p5_00003.jpg';
+import Foto4 from './livro_p5_00004.jpg';
+import Foto5 from './livro_p5_00005.jpg';
+import Foto6 from './livro_p5_00006.jpg';
+import Foto7 from './livro_p5_00007.jpg';
+import Foto8 from './livro_p5_00008.jpg';
+import Foto9 from './livro_p5_00009.jpg';
+import Foto10 from './livro_p5_00010.jpg';
+import Foto11 from './livro_p5_00011.jpg';
+import Foto12 from './livro_p5_00012.jpg';
+import Foto13 from './livro_p5_00013.jpg';
+import Foto14 from './livro_p5_00014.jpg';
+import Foto15 from './livro_p5_00015.jpg';
+import Foto16 from './livro_p5_00016.jpg';
+import Foto17 from './livro_p5_00017.jpg';
+import Foto18 from './livro_p5_00018.jpg';
+import Foto19 from './livro_p5_00019.jpg';
+import Foto20 from './livro_p5_00020.jpg';
+import Foto21 from './livro_p5_00021.jpg';
+import Foto22 from './livro_p5_00022.jpg';
+import Foto23 from './livro_p5_00023.jpg';
+import Foto24 from './livro_p5_00024.jpg';
 
-export { Foto1, Foto2, Foto3, Foto4 };
+
+export { 
+    Foto1, 
+    Foto2,
+    Foto3, 
+    Foto4, 
+    Foto5, 
+    Foto6, 
+    Foto7, 
+    Foto8, 
+    Foto9, 
+    Foto10,
+    Foto11,
+    Foto12,
+    Foto13,
+    Foto14,
+    Foto15,
+    Foto16,
+    Foto17,
+    Foto18,
+    Foto19,
+    Foto20,
+    Foto21,
+    Foto22,
+    Foto23,
+    Foto24
+};
