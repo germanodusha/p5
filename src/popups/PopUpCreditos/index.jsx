bc84428c11d30849cc7754932729479a09f72e28
unknown <adrianogrego1@gmail.com>
2021-12-23 20:15:37 -0300

    Corrigindo caminho de artistasEcuradores

--- src/popups/PopUpCreditos/index.jsx
+++ src/popups/PopUpCreditos/index.jsx
@@ -8,30 +8,19 @@ import {
   Bolinha,
   Fechar,
 } from "./estilo";
-<<<<<<< HEAD
 
 import fechar from "../../imagens/icones/x_preto.svg";
 
-=======
-import fechar from "../../imagens/icones/x_preto.svg"
->>>>>>> 3c91be0ec78c9ac500277224a6fd96dde784a5d3
 import database from "../../database";
 
 function PopUpCreditos({ fClick }) {
   const fichaTecnica = database.ptbr.info.fichaTecnica;
-<<<<<<< HEAD
-  const artistasECuradores = database.ptbr.principal.artistasECuradores;
+  const artistasECuradores = database.ptbr.info.artistasECuradores;
   const db = database.ptbr.info.sobrePeriscopio;
   const [estado, setEstado] = useState(true);
-=======
-  const artistasECuradores = database.ptbr.info.artistasECuradores;
-  const sobre = database.ptbr.info.sobrePeriscopio;
-  const [estado,setEstado] = useState(true);
->>>>>>> 3c91be0ec78c9ac500277224a6fd96dde784a5d3
 
   return (
     <Conteiner>
-<<<<<<< HEAD
       <Content className="startRow">
         {estado ? (
           <>
@@ -66,32 +55,6 @@ function PopUpCreditos({ fClick }) {
                   </>
                 ))}
               </Info>
-=======
-      <Content className="centerRow">
-        {estado ? (<> 
-        <Info id="agradecimentos" className="startRow">
-          <h3>{sobre.titulo}</h3>
-          <p>{sobre.texto}</p>
-        </Info>
-        <CaixaTexto className="startRow" >
-          <Info id="artistasECuradores">
-           <p>{artistasECuradores}</p>
-          </Info>   
-        </CaixaTexto></>):(<><CaixaTexto className="startRow" >
-          <Info>
-            {fichaTecnica.parte1.map((e) => (
-              <>
-                <span>{e.titulo}</span>
-                <span>{e.autoria}</span>
-              </>
-            ))}
-          </Info>
-          
-          <Info>{fichaTecnica.parte2.slice(0,7).map((e) => (
-            
-             <> <span>{e.titulo}</span>
-              <span>{e.autoria}</span></>
->>>>>>> 3c91be0ec78c9ac500277224a6fd96dde784a5d3
             
 
             <Info id="agradecimentos" >
