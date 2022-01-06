5ed40d1bce87472cfa9c4483fdb27d81ab64370f
Jainy Gomes <andreajgsouza@hotmail.com>
2022-01-06 18:42:41 -0300

    mudando de novo sobre/artistas para agentes

--- src/database.js
+++ src/database.js
@@ -262,7 +262,7 @@ const database = {
             artistasECuradores: 'Alice Ricci, Amilcar de Castro, Ana Linnemann, Andrea Brown, Andréa Brown, Andrea Gomes, Avilmar Maia, Beatriz de Almeida Magalhães, Binho Barreto, Bruno Faria, Bruno Palazzo, Camila Lacerda, Cristina Marigo, Daniel Bilac, Daniel de Paula, Daniel Steegmann Mangrané, Daniela Moser, Deyson Gilbert, Éder Oliveira, Eduardo Hargreaves, Ernesto Neto, Erre Erre, Fábio Baroli, Fábio Tremonte, Fabíola Moulin, Fabíola Tasca, Felipe Scovino, Flávia Péret, Flávio Cerqueira, Francisco Lara Rezende, Frederico Morais, Germano Dushá, Gisele Camargo, Guto Lacaz, Hélio Menezes, Henrique Detomi, Iara Marques, Iberê Camargo, Ina Gouveia, Ivens Machado, Janaina Wagner, José Lara, Juliana Gontijo, Júlio Martins, Katia Canton, Lais Myrrha, Laura Berbert, Letícia Grandinetti, Lorenzato, Luana Vitra, Lucas Dupin, Lucas ­Kröeff­, Lucas Simões, Marc Davi, ­Marcel­ Diogo, Marcela Novaes, Marcelo Drummond, Márcia Guimarães, Márcio Diegues, Marco Maria Zanin, Marco Sem S, Marcone Moreira, Marconi Drummond, Maria Palmeiro, Maria Thereza Alves, Marina Câmara, Maurício Meirelles, Mauro Restiffe, Nydia Negromonte, Patrícia Leite, Paula Borghi, Paula Huven, Paula Sampaio, Paulo Baptista, Pedro Motta, Rachel Cecília de Oliveira, Rafael RG, Randolpho Lamonier, Renata De Bonis, Ricardo Carioba, Rodrigo Albert, Rodrigo Braga, Rodrigo Cass, Selma Parreira, Sérgio Martins, TRAPLEV, Umberto Costa Barros, Victor Galvão, Wilson Baptista, Wilson Lázaro.'
             , botoes: {
                 fichaTecnica: 'Ficha Técnica',
-                sobre: 'Sobre/Artistas'
+                sobre: 'Sobre/Agentes'
             }
         },
         botoes: {
@@ -548,7 +548,7 @@ const database = {
             artistasECuradores: 'Alice Ricci, Amilcar de Castro, Ana Linnemann, Andrea Brown, Andréa Brown, Andrea Gomes, Avilmar Maia, Beatriz de Almeida Magalhães, Binho Barreto, Bruno Faria, Bruno Palazzo, Camila Lacerda, Cristina Marigo, Daniel Bilac, Daniel de Paula, Daniel Steegmann Mangrané, Daniela Moser, Deyson Gilbert, Éder Oliveira, Eduardo Hargreaves, Ernesto Neto, Erre Erre, Fábio Baroli, Fábio Tremonte, Fabíola Moulin, Fabíola Tasca, Felipe Scovino, Flávia Péret, Flávio Cerqueira, Francisco Lara Rezende, Frederico Morais, Germano Dushá, Gisele Camargo, Guto Lacaz, Hélio Menezes, Henrique Detomi, Iara Marques, Iberê Camargo, Ina Gouveia, Ivens Machado, Janaina Wagner, José Lara, Juliana Gontijo, Júlio Martins, Katia Canton, Lais Myrrha, Laura Berbert, Letícia Grandinetti, Lorenzato, Luana Vitra, Lucas Dupin, Lucas ­Kröeff­, Lucas Simões, Marc Davi, ­Marcel­ Diogo, Marcela Novaes, Marcelo Drummond, Márcia Guimarães, Márcio Diegues, Marco Maria Zanin, Marco Sem S, Marcone Moreira, Marconi Drummond, Maria Palmeiro, Maria Thereza Alves, Marina Câmara, Maurício Meirelles, Mauro Restiffe, Nydia Negromonte, Patrícia Leite, Paula Borghi, Paula Huven, Paula Sampaio, Paulo Baptista, Pedro Motta, Rachel Cecília de Oliveira, Rafael RG, Randolpho Lamonier, Renata De Bonis, Ricardo Carioba, Rodrigo Albert, Rodrigo Braga, Rodrigo Cass, Selma Parreira, Sérgio Martins, TRAPLEV, Umberto Costa Barros, Victor Galvão, Wilson Baptista, Wilson Lázaro.'
             , botoes: {
                 fichaTecnica: 'Credits',
-                sobre: 'About/Artists'
+                sobre: 'About/Agents'
             }
         },
         botoes: {
